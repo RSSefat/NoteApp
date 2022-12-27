b280da699f0a9f5606b097500cddc633bb950d3e
rs sefat <sefathasan4@gmail.com>
2022-12-27 10:39:36 +0600

    practice project done

--- @latest/src/App.vue
+++ @latest/src/App.vue
@@ -8,7 +8,8 @@
    const notes = ref([])  
    const noteTitle = ref()  
    const openCard = ref(false) 
-   const edit = ref(false) 
+   const cardBtn = ref(false) 
+   const showIcon = ref(false) 
 
   
 
@@ -20,6 +21,8 @@
       date: new Date(),
       backgroundColor: "hsl(" + Math.random() * 360 + ", 100%, 70%)",
       openCard: openCard.value,
+      cardBtn: cardBtn.value,
+      showIcon: showIcon.value,
       Title: noteTitle.value,
      
         });
@@ -46,6 +49,7 @@
         <textarea v-model="newNote" name="note" id="note" cols="1" rows="10"></textarea>
         <span>
         <button  @click="addNote()" v-if="!edit">Add Note</button>
+      
       </span>
         <button @click="showModal = false" class="close">Close</button>
       </div>
@@ -57,18 +61,29 @@
         <button @click="showModal=true">+</button>
       </header>
       <div class="cards-container" >
-        <div v-for="(note , index) in notes" :key="note.id"  :class="{toggleCard: note.openCard}" :id="note.id"  class="card" :style="{backgroundColor: note.backgroundColor}">
+        <div v-for="(note , index) in notes" :key="note.id" @mouseover="note.showIcon = true" @mouseleave="note.showIcon = false " :class="{toggleCard: note.openCard}" :id="note.id"  class="card" :style="{backgroundColor: note.backgroundColor}">
           <h3>{{note.Title}}</h3>
           <p class="main-text"> {{note.text}}</p>
-          <div>
-          <span class="date">{{note.date.toLocaleDateString("en-US")}}</span>
-            <button @click="deleteNote(index)">delete</button>
+      
+          <div  class="cardBtn" :class="{cardButton: note.cardBtn}" v-show="note.showIcon">
+         
+            <span class="date">{{note.date.toLocaleDateString("en-US")}}</span>
+          <div >
+            <button @click="deleteNote(index)">
+              <font-awesome-icon icon="fa-solid fa-trash-can" />
+            </button>
            
             <span @click="note.openCard =!note.openCard" >
-              <button v-if="!note.openCard">View</button>
-              <button  v-if="note.openCard">x</button>
+              <button v-if="!note.openCard">
+                <font-awesome-icon icon="fa-solid fa-eye" />
+              </button>
+              <button  v-if="note.openCard">
+                <font-awesome-icon icon="fa-solid fa-eye-slash" />
+              </button>
             </span>
           </div>
+          </div>
+ 
           
         </div>
   
@@ -135,6 +150,8 @@
   .date{
     font-size: 10.5px;
     font: bold;
+    display: flex;
+
   }
   .overlay{
 
@@ -180,11 +197,24 @@
    height: 60%;
    width: 100vh;
    display:flex;
-    align-items: flex-start;
+   
     justify-content: space-between;
   }
   .toggleCard h3{
     margin-bottom: -10%;
   }
+  .cardBtn,   .cardButton{
+    display: flex;
+    justify-content: space-between;
+    
+    
+  
+  }
+.cardBtn div button ,.cardbtn span button{
+  border: none;
+  background: transparent;
+
+}
+
 
 </style>>
